5bb6ca561b59bc98f5197211fa0f54a099fd0876
Alex Manochio <cajm23331@gmail.com>
2026-09-13 18:30:40 -0400

    Initial Commit

---
+++ HouseLearning/HouseLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseLearningApp.swift
+//  HouseLearning
+//
+//  Created by Admin on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct HouseLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
